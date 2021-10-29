84a1148ead4d07bd0eff4e36e3f18a42ba1832c1
bioted <tgarc11@lsu.edu>
2021-10-29 12:07:55 -0500

    Added initial markdown for R analyses

---
+++ analyses/TGarciaAroca_XnecrophoraSecondaryMetabolites.Rmd.R
@@ -0,0 +1,23 @@
+---
+title: Statistical analyses and plotting for Xylaria necrophora secondary metabolites experiments
+author: Teddy Garcia-Aroca
+date: October 29, 2021
+output: html_document
+---
+
+Install and load required packages
+
+```{r}
+library(agricolae)
+library(dplyr)
+library(plyr)
+require(ggplot2)
+library(readr)
+library("ggpubr")
+library("car")
+library(rcompanion)
+#install.packages("tidyverse")
+library(tidyverse)
+library(reshape)
+```
+
